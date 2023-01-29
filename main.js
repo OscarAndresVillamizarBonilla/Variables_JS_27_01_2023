bbf51ea600ce73a4e8c2a5ccc2eb88a6cd6ef4a7
OscarAndresVillamizarBonilla <andrex.villamizar1@gmail.com>
2023-01-29 15:19:03 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+
+
+
+//let num1 = 50;
+//let num2 = 50;
+//console.log(num1+num2);
+
+
+
+let nombre = new String("miguel");
+let nombre1 = "miguel";
+
+
+let Tipo_Edificio = "comercial";
+let Cantidad_Pisos = 10;
+var Zona = { Tipo_Edificio: "residencial", Cantidad_Pisos: 7};
+
+console.log( `El tipo de edificio a construir es ${Tipo_Edificio} , `);
+console.log(`y la cantidad de pisos del edificio ${Tipo_Edificio} son ${Cantidad_Pisos}`);
+console.log(Zona);
+// String Cadenas de texto.
+// Number Valores numéricos.
+// Boolean Representa una entidad lógica y puede tener dos valores: true y false.
+// Object Un valor en memoria al que podemos acceder por un identificador.
